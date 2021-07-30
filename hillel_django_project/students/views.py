16b618685a9fef0c6ebb39da9b44837fdd3038e7
Michael Frolov <moyshedev@gmail.com>
2021-07-30 20:23:43 +0300

    Added make_student function in the views.py file

--- hillel_django_project/students/views.py
+++ hillel_django_project/students/views.py
@@ -1,6 +1,18 @@
 from django.http import HttpResponse
+from faker import Faker
+
+from .models import Student
 
 
-def home(request):
+def make_student():
+    """Generate student and added him in DataBase"""
+    fake = Faker()
+    student = Student.objects.create(first_name=fake.first_name(),
+                                     last_name=fake.last_name(),
+                                     age=fake.random_int(16, 45))
+    return student
+
+
+def home(request) -> HttpResponse:
     return HttpResponse('<h1 align=center>Welcome to "Hillel Homework Django Project"</h1>'
                         '<p align=center>by Michail Frolov</p>')
