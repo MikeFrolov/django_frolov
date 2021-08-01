246c50f778897c786b72dfe35a20215e833d71d0
Michael Frolov <moyshedev@gmail.com>
2021-08-01 14:56:29 +0300

    Renamed project, Changed the structure of the project, Changed the nesting of the app in the project

--- Django_Frolov/asgi.py
+++ Django_Frolov/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for hillel_django_project project.
+ASGI config for Django_Frolov project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'hillel_django_project.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Django_Frolov.settings')
 
 application = get_asgi_application()
--- Django_Frolov/urls.py
+++ Django_Frolov/urls.py
@@ -1,4 +1,4 @@
-"""hillel_django_project URL Configuration
+"""Django_Frolov URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.2/topics/http/urls/
