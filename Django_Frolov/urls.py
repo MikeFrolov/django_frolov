5f79745924645aaf6b4b808c1df7214cf7003548
Michael Frolov <moyshedev@gmail.com>
2021-08-01 20:27:11 +0300

    The list of imports was rewritten to meet the requirements of the pep-8 standard

--- Django_Frolov/urls.py
+++ Django_Frolov/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-from students import views as student_views
 from group import views as group_views
+
+from students import views as student_views
+
 from teachers import views as teacher_views
 
 urlpatterns = [
