3537914b05e81e402b313208d9a973688547a999
Michael Frolov <moyshedev@gmail.com>
2021-08-01 23:50:32 +0300

    Added new field in model 'teacher', remaked migrations

--- teachers/models.py
+++ teachers/models.py
@@ -5,3 +5,4 @@ class Teacher(models.Model):
     first_name = models.CharField(max_length=200)
     last_name = models.CharField(max_length=200)
     discipline = models.CharField(max_length=200)
+    age = models.IntegerField(default=23)
