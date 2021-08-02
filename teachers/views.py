77d91cf62a9a9713badaa2ca410946bbb6673283
Michael Frolov <moyshedev@gmail.com>
2021-08-02 10:55:41 +0300

    Created new view-function 'get_teachers'

--- teachers/views.py
+++ teachers/views.py
@@ -3,10 +3,37 @@ from django.http import HttpResponse
 from .models import Teacher
 
 
-def teachers(request):
+def get_all_teachers(request):
+    """
+    List all teachers from database
+    :param request: None
+    :return: HttpResponse
+    """
     teachers_list = Teacher.objects.all()
     output = ''.join(
-        [f"<p>Teacher {teacher.id}: {teacher.first_name} {teacher.last_name} - "
+        [f"<p>Teacher {teacher.id}: {teacher.first_name} {teacher.last_name}, {teacher.age} years old - "
          f"teaches: '{teacher.discipline}' course;</p>" for teacher in teachers_list]
     )
     return HttpResponse(output)
+
+
+def get_filtered_teachers(request):
+    """
+    List teachers with filtering functionality by fields age, first_name, last_name.
+    :param request: id, first_name, last_name, age, discipline
+    :return: HttpResponse
+    """
+
+    filter_parameters = {p: v for p, v in request.GET.items()}
+
+    if not filter_parameters:  # If no filtering parameters are entered
+        return get_all_teachers(request)  # List all teachers from database
+    else:
+        filtered_teachers = [obj for obj in Teacher.objects.filter(**filter_parameters)]
+
+        output = ''.join(
+            [f"<p>Teacher {teacher.id}: {teacher.first_name} {teacher.last_name}, {teacher.age} years old - "
+             f"teaches: '{teacher.discipline}' course;</p>" for teacher in filtered_teachers])
+        return HttpResponse(output)
+
+    # TODO: Think about how not to get an error if a filter field is entered, but its value is not entered
