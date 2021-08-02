f6fbad5ce8da2791f39a443c25e4193f465bd22f
Michael Frolov <moyshedev@gmail.com>
2021-08-02 19:08:10 +0300

    Created new custom command 'generate_teachers'

---
+++ teachers/management/commands/generate_teachers.py
@@ -0,0 +1,21 @@
+from django.core.management.base import BaseCommand
+
+from faker import Faker
+
+from teachers.models import Teacher
+
+
+class Command(BaseCommand):
+    help = 'Generates random teachers base on input amount'
+
+    def add_arguments(self, parser):
+        parser.add_argument('number_of_teachers', nargs='+', type=int, default=100)
+
+    def handle(self, *args, **options):
+        number = options['number_of_teachers']
+        fake = Faker()
+        for i in range(*number):
+            Teacher.objects.create(first_name=fake.first_name(),
+                                   last_name=fake.last_name(),
+                                   age=fake.random_int(23, 85))
+        self.stdout.write(self.style.SUCCESS(f'Successfully created {str(*number)} new teachers on the database'))
