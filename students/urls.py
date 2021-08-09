5db229a5c2920fcb9e906805ea9faa28ff500d63
Michael Frolov <moyshedev@gmail.com>
2021-08-09 20:52:54 +0300

    Rewrote the functions for generating students to display the entire list after creating a new one

--- students/urls.py
+++ students/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 
 from .views import (
     create_student_form,
+    edit_student_form,
     generate_student,
     generate_students,
-    list_all_students,
     list_filtered_students
 )
 
@@ -13,5 +13,7 @@ urlpatterns = [
     path('generate-student/', generate_student, name='generate-student'),
     path('generate-students/', generate_students, name='generate-students'),
     path('create_student_form/', create_student_form, name='create-student-form'),
+    path('edit_student_form/<int:student_id>', edit_student_form, name='edit-student-form'),
     path('list_students/', list_filtered_students, name='list-filtered-students'),
+
 ]
