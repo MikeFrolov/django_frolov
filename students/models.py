d2bdb5e408788c39e46b5ae49c4e1de4e09d8896
Michael Frolov <moyshedev@gmail.com>
2021-09-01 21:18:46 +0300

    Added a PHONE field to the teacher model, created a phone number generator when generating teacherss, added a phone number field validation

--- students/models.py
+++ students/models.py
@@ -8,9 +8,5 @@ class Student(models.Model):
     age = models.IntegerField(default=16)
     phone = PhoneNumberField(null=False, blank=True, unique=True, default=None)
 
-    def clean(self):
-        self.first_name = self.first_name.capitalize
-        self.last_name = self.last_name.capitalize
-
     def __str__(self):
         return f"{self.first_name} {self.last_name} {self.age} {self.phone}"
