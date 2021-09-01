e34cd30b057fc0b197746492f52a66a39689207b
Michael Frolov <moyshedev@gmail.com>
2021-09-01 20:35:13 +0300

    Added a PHONE field to the student model, created a phone number generator when generating students, added a phone number field validation

--- students/models.py
+++ students/models.py
@@ -6,7 +6,11 @@ class Student(models.Model):
     first_name = models.CharField(max_length=200)
     last_name = models.CharField(max_length=200)
     age = models.IntegerField(default=16)
-    phone = PhoneNumberField(null=False, blank=False, unique=True, default=None)
+    phone = PhoneNumberField(null=False, blank=True, unique=True, default=None)
+
+    def clean(self):
+        self.first_name = self.first_name.capitalize
+        self.last_name = self.last_name.capitalize
 
     def __str__(self):
-        return f"{self.first_name} {self.last_name} {self.age}"
+        return f"{self.first_name} {self.last_name} {self.age} {self.phone}"
