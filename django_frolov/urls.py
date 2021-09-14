5cd74731b904b62a6734ba0ea722a719a7ab5e77
Michael Frolov <moyshedev@gmail.com>
2021-09-14 11:38:18 +0300

    Redesigned base page with bootstrap.

--- django_frolov/urls.py
+++ django_frolov/urls.py
@@ -23,7 +23,7 @@ from .views import home
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('home/', home, name='home'),
+    path('', home, name='home'),
     path('', include('students.urls')),
     path('', include('group.urls')),
     path('', include('teachers.urls')),
--- teachers/urls.py
+++ teachers/urls.py
@@ -4,6 +4,7 @@ from .views import (
     create_teacher_form,
     delete_teacher,
     edit_teacher_form,
+    generate_teacher,
     generate_teachers,
     list_filtered_teachers,
 
@@ -12,6 +13,7 @@ from .views import (
 
 urlpatterns = [
     path('list_teachers/', list_filtered_teachers, name='list-filtered-teachers'),
+    path('generate-teacher/', generate_teacher, name='generate-teacher'),
     path('generate-teachers/', generate_teachers, name='generate-teachers'),
     path('create_teacher_form/', create_teacher_form, name='create-teacher-form'),
     path('edit_teacher_form/<int:teacher_id>', edit_teacher_form, name='edit-teacher-form'),
