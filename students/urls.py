d749ef689c4a797d831f1f7ced1b45053d73af7b
Michael Frolov <moyshedev@gmail.com>
2021-09-17 12:12:33 +0300

    Added url 'generate_students_form/' in the students/urls.py

--- students/urls.py
+++ students/urls.py
@@ -6,6 +6,7 @@ from .views import (
     edit_student_form,
     generate_student,
     generate_students,
+    generate_students_form,
     list_filtered_students,
 )
 
@@ -14,6 +15,7 @@ urlpatterns = [
     path('list_students/', list_filtered_students, name='list-filtered-students'),
     path('generate-student/', generate_student, name='generate-student'),
     path('generate-students/', generate_students, name='generate-students'),
+    path('generate_students_form/', generate_students_form, name='generate-students-form'),
     path('create_student_form/', create_student_form, name='create-student-form'),
     path('edit_student_form/<int:student_id>', edit_student_form, name='edit-student-form'),
     path('delete_student/<int:student_id>', delete_student, name='delete-student'),
