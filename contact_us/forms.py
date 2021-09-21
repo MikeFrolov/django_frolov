bf4ae794624a46dad32e8184a14058522608e017
Michael Frolov <moyshedev@gmail.com>
2021-09-21 21:24:32 +0300

    Created new app 'contact_us', set up every files for sending emails from the application, creation new model, view function and task

--- contact_us/forms.py
+++ contact_us/forms.py
@@ -1,9 +1,13 @@
 from django import forms
 
-from .models import ContactUs
 
+class ContactUsForm(forms.Form):
+    title = forms.CharField(label='Title:', max_length=100,
+                            widget=forms.TextInput(attrs={'class': 'form-control'}))
+    message = forms.CharField(label='Message:', max_length=200,
+                              widget=forms.Textarea(attrs={'class': 'form-control', "rows": 5}))
+    email_from = forms.CharField(label='Email from:', max_length=100,
+                                 widget=forms.TextInput(attrs={'class': 'form-control'}))
 
-class ContactUsFormFormModel(forms.ModelForm):
-    class Meta:
-        model = ContactUs
-        fields = ['title', 'message', 'email_from']
+    def __str__(self):
+        return self.title, self.message, self.email_from
