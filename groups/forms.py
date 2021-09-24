b5001fc87435f4cdcb7dfbc8250e20e9df578621
Michael Frolov <moyshedev@gmail.com>
2021-09-24 12:39:36 +0300

    Made amendments related to renaming app 'group' to 'groups'

--- groups/forms.py
+++ groups/forms.py
@@ -4,13 +4,12 @@ from .models import Group
 
 """
 class GroupForm(forms.Form):
-    group_name = forms.CharField(label='Group name', required=True, max_length=200)
-    faculty_name = forms.CharField(label='Faculty name', required=True, max_length=200)
-    number_of_students = forms.IntegerField(label='Number of students')
+    group_name = forms.CharField(label='Group name', required=True, max_length=40)
+    discipline = forms.CharField(label='Discipline', required=True, max_length=40)
 """
 
 
 class GroupFormFormModel(forms.ModelForm):
     class Meta:
         model = Group
-        fields = ['group_name', 'faculty_name', 'number_of_students']
+        fields = ['group_name', 'discipline']
--- groups/models.py
+++ groups/models.py
@@ -2,9 +2,8 @@ from django.db import models
 
 
 class Group(models.Model):
-    group_name = models.CharField(max_length=60)
-    faculty_name = models.CharField(max_length=200)
-    number_of_students = models.IntegerField(default=20)
+    group_name = models.CharField(max_length=40)
+    discipline = models.CharField(max_length=40)
 
     def __str__(self):
-        return f"{self.group_name} {self.faculty_name} {self.number_of_students}"
+        return f"{self.group_name} {self.discipline}"
