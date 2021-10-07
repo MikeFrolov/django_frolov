ba56802e2ab6d737a2d9a476103a431e51af56e4
Michael Frolov <moyshedev@gmail.com>
2021-10-07 14:16:55 +0300

    Rewritten all view functions for classes in the 'groups' application

--- groups/urls.py
+++ groups/urls.py
@@ -1,17 +1,17 @@
 from django.urls import path
 
 from .views import (
-    create_group_form,
-    delete_group,
-    edit_group_form,
-    list_groups,
+    CreateGroupFormView,
+    DeleteGroupView,
+    EditGroupFormView,
+    GroupsListView,
 )
 
 
 urlpatterns = [
-    path('list_groups/', list_groups, name='list-groups'),
-    path('create_group_form/', create_group_form, name='create-groups-form'),
-    path('edit_group_form/<int:group_id>', edit_group_form, name='edit-group-form'),
-    path('delete_group/<int:group_id>', delete_group, name='delete-group')
+    path('list_groups/', GroupsListView.as_view(), name='list-groups'),
+    path('create_group_form/', CreateGroupFormView.as_view(), name='create-groups-form'),
+    path('edit_group_form/<int:group_id>', EditGroupFormView.as_view(), name='edit-group-form'),
+    path('delete_group/<int:pk>', DeleteGroupView.as_view(), name='delete-group')
 
 ]
