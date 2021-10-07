bf4fc3796ffc388c750323844c9cf3ccf96d661c
Michael Frolov <moyshedev@gmail.com>
2021-10-07 09:29:24 +0300

    Rewritten all view functions for classes in the 'general' application

--- general/views.py
+++ general/views.py
@@ -3,3 +3,6 @@ from django.views.generic.base import TemplateView
 
 class HomeView(TemplateView):
     template_name = 'index.html'
+    # Overriding the context variable to be used in the {% for group in group_list %} block
+    # instead of {% group in object_list %}
+    context_object_name = 'group_list'
